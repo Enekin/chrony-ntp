965dbb72e5a9f97d60505f2e1039a270e2f20979
Sylvain Arrambourg <sar@make.org>
2017-07-10 10:38:00 +0200

    chore: set make.org copyright, fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,14 @@
-name 'test-cookbook'
+name 'chrony-ntp'
 maintainer 'Make.org'
 maintainer_email 'sre@make.org'
 license 'Apache-2.0'
-description 'Cookbook used to test some chef behaviors'
+description 'Install and configure chrony daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://gitlab.com/chef-platform/test-cookbook'
-issues_url 'https://gitlab.com/chef-platform/test-cookbook/issues'
+source_url 'https://gitlab.com/chef-platform/chrony-ntp'
+issues_url 'https://gitlab.com/chef-platform/chrony-ntp/issues'
 version '1.0.0'
 
-supports 'centos', '>= 7.1'
-supports 'arch'
+supports 'centos', '>= 7.3'
 supports 'debian', '>= 8'
 
 chef_version '>= 12.19'
